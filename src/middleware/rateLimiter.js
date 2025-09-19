ef253353bcabd1babf8c4ca6bbfbd61390373649
Sahil Dolia <sahil.dolia@sourcemash.com>
2025-09-19 15:29:07 +0530

    changes

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const emailRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 30, // Limit each IP to 3 requests per window
+  windowMs: 15 * 60 * 1000,
+  max: 3, 
   message: {
     success: false,
     message: 'Too many contact attempts. Please try again later.'
